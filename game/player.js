74dfc749f1682e28f9ae603d035460874b388d8c
Maxime Lizandier <maxime.lizandier@epita.fr>
2021-09-22 19:29:56 +0200

    README 1 -> turn right

--- game/player.js
+++ game/player.js
@@ -52,7 +52,7 @@ Player.prototype.displayInfo = function () {
 
 Player.prototype.turnRight = function (angle) {
     this.direction += angle;
-    this.graphic.rotateOnAxis(new THREE.Vector3(0,0,1), +angle);
+    this.graphic.rotateOnAxis(new THREE.Vector3(0,0,1), -angle); // README 1 -> turn right
 };
 
 Player.prototype.turnLeft = function (angle) {
@@ -78,7 +78,7 @@ Player.prototype.move = function () {
 
     this.graphic.position.x = this.position.x;
     this.graphic.position.y = this.position.y;
-    
+
     light1.position.x = this.position.x;
     light1.position.y = this.position.y;
    //light1.position.z = this.graphic.position.z + 500;
